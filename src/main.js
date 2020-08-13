6956b954cd76e5f60e3706506fad08101693aafe
Ruta Yu <flyling503@gmail.com>
2020-08-14 00:34:08 +0800

    no message

--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
 import Vue from 'vue'
+import $ from 'jquery'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import Loading from 'vue-loading-overlay'
+import 'vue-loading-overlay/dist/vue-loading.css'
+import 'bootstrap'
+import Vue2Editor from 'vue2-editor'
+
 import App from './App.vue'
 import router from './router'
 
 Vue.config.productionTip = false
+
+window.$ = $
+
+Vue.component('Loading', Loading)
+Vue.use(VueAxios, axios)
+Vue.use(Vue2Editor)
 
 new Vue({
   router,
--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,45 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'root',
+    redirect: '/login'
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/login',
+    name: 'Login',
+    component: () => import('../views/Login.vue')
+  },
+  {
+    path: '/admin',
+    name: 'Admin',
+    component: () => import('../views/Dashboard.vue'),
+    children: [
+      {
+        path: 'products',
+        name: 'Products',
+        component: () => import('../views/dashboard/Products.vue')
+      },
+      {
+        path: 'orders',
+        name: 'Orders',
+        component: () => import('../views/dashboard/Orders.vue')
+      },
+      {
+        path: 'coupons',
+        name: 'Coupons',
+        component: () => import('../views/dashboard/Coupons.vue')
+      },
+      {
+        path: 'images',
+        name: 'Images',
+        component: () => import('../views/dashboard/Images.vue')
+      }
+    ]
   }
 ]
 
